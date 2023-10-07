7ebbe6949a9498e8840ffff72c408b2d82f4bc4e
Felix Hake <fh@ciblu-software.de>
2023-10-07 20:47:26 +0200

    Convert to file scoped namespace

--- Client/MauiProgram.cs
+++ Client/MauiProgram.cs
@@ -1,24 +1,23 @@
 ﻿using DevExpress.Maui;
 
-namespace Client
-{
-    public static class MauiProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .UseDevExpress(useLocalization: true)
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("univia-pro-regular.ttf", "Univia-Pro");
-                    fonts.AddFont("roboto-bold.ttf", "Roboto-Bold");
-                    fonts.AddFont("roboto-regular.ttf", "Roboto");
-                });
+namespace Client;
 
-            return builder.Build();
-        }
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseDevExpress(useLocalization: true)
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("univia-pro-regular.ttf", "Univia-Pro");
+                fonts.AddFont("roboto-bold.ttf", "Roboto-Bold");
+                fonts.AddFont("roboto-regular.ttf", "Roboto");
+            });
+
+        return builder.Build();
     }
 }
